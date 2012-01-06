ad648b249273503ce4d2508261077e456f2cf3d4
James Halliday <mail@substack.net>
2012-01-06 06:04:59 -0800

    clean up that pesky interval

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ var cube = require('surrender-cube');
 var charm = require('charm');
 
 http.createServer(function (req, res) {
-    cube({ charm : charm(req, res) });
+    var iv = cube({ charm : charm(req, res) });
+    
+    req.connection.on('end', function () {
+        clearInterval(iv);
+    });
 }).listen(8080);
